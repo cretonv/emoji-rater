e8912d0bb675dcaf52de9669575683732df201b4
Vincent <vincecreton@hotmail.fr>
2022-01-25 15:49:40 +0100

    Add Vote entity 🤛

--- src/Entity/Rating.php
+++ src/Entity/Rating.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\RatingRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: RatingRepository::class)]
@@ -25,6 +27,14 @@ class Rating
 
     #[ORM\Column(type: 'json', nullable: true)]
     private $metadata = [];
+
+    #[ORM\OneToMany(mappedBy: 'rating', targetEntity: Vote::class)]
+    private $votes;
+
+    public function __construct()
+    {
+        $this->votes = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -75,6 +85,36 @@ class Rating
     public function setMetadata(array $metadata): self
     {
         $this->metadata = $metadata;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Vote[]
+     */
+    public function getVotes(): Collection
+    {
+        return $this->votes;
+    }
+
+    public function addVote(Vote $vote): self
+    {
+        if (!$this->votes->contains($vote)) {
+            $this->votes[] = $vote;
+            $vote->setRating($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVote(Vote $vote): self
+    {
+        if ($this->votes->removeElement($vote)) {
+            // set the owning side to null (unless already changed)
+            if ($vote->getRating() === $this) {
+                $vote->setRating(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Vote.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoteRepository::class)]
+class Vote
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isUp;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $authorUserEmail;
+
+    #[ORM\ManyToOne(targetEntity: Rating::class, inversedBy: 'votes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $rating;
+
+    #[ORM\Column(type: 'json', nullable: true)]
+    private $metadata = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIsUp(): ?bool
+    {
+        return $this->isUp;
+    }
+
+    public function setIsUp(bool $isUp): self
+    {
+        $this->isUp = $isUp;
+
+        return $this;
+    }
+
+    public function getAuthorUserEmail(): ?string
+    {
+        return $this->authorUserEmail;
+    }
+
+    public function setAuthorUserEmail(string $authorUserEmail): self
+    {
+        $this->authorUserEmail = $authorUserEmail;
+
+        return $this;
+    }
+
+    public function getRating(): ?Rating
+    {
+        return $this->rating;
+    }
+
+    public function setRating(?Rating $rating): self
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getMetadata(): ?array
+    {
+        return $this->metadata;
+    }
+
+    public function setMetadata(?array $metadata): self
+    {
+        $this->metadata = $metadata;
+
+        return $this;
+    }
+}
