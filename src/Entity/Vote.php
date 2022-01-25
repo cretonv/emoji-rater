164b7401b3d6e677988502fa86381d56c68bb4e4
Léo Boyer <contact@leoboyer.fr>
2022-01-25 16:20:37 +0100

    Added api platform resources to every entity 🪆

--- src/Entity/Vote.php
+++ src/Entity/Vote.php
@@ -2,9 +2,11 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\VoteRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: VoteRepository::class)]
 class Vote
 {
