2a4af902c3f7f1d0d0c5778b225b574386b85624
Vincent <vincecreton@hotmail.fr>
2022-01-25 15:36:55 +0100

    Add Website entity 🏎

---
+++ src/Entity/Website.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WebsiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WebsiteRepository::class)]
+class Website
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'text')]
+    private $domain;
+
+    #[ORM\Column(type: 'text')]
+    private $token;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDomain(): ?string
+    {
+        return $this->domain;
+    }
+
+    public function setDomain(string $domain): self
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+}
